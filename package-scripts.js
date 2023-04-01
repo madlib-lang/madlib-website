d088c3602aab4c8dc630027177830067d087f4cf
Brekk <brekk@brekkbockrath.com>
2023-04-01 14:28:06 -0700

    April 🐟 Round 3 (#9)

--- package-scripts.js
+++ package-scripts.js
@@ -123,6 +123,7 @@ module.exports = {
         "cp src/client/index.html build/public/",
         "cp src/client/robots.txt build/public/",
         "cp -R src/client/assets build/public/",
+        "cp -R src/client/assets build/public/assets",
         "cp src/client/*.json build/public/",
         `ls build/public/`,
         "nps server.prod.build",
